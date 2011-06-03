00f890490b6daf6e4c0ce538ddca03d2a9fe9001
paradigm <danthau@gmail.com>
2011-06-03 07:23:43 -0400

    Removed watchfile, spaced "$CURSORPOS $FILENAME"

--- coordsearch.sh
+++ coordsearch.sh
@@ -6,7 +6,7 @@ OLDCURSOR=""
 while true; do
 	CURSORPOS=$(vim --servername "$VIMSERVER" --remote-expr 'line(".").":".col(".")')
 	if [ "$CURSORPOS" != "$OLDCURSOR" ]; then
-		xdvi -watchfile 1 -editor "vim --servername $VIMSERVER --remote +%l %f" -sourceposition "$CURSORPOS$FILENAME" "$OUTNAME"
+		xdvi -editor "vim --servername $VIMSERVER --remote +%l %f" -sourceposition "$CURSORPOS $FILENAME" "$OUTNAME"
 		OLDCURSOR=$CURSORPOS
 	fi
 	sleep .10s
